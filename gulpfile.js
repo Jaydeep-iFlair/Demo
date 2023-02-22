c439dfb26ca990f432a1e3c818143430c5c8f442
Dipen Modi (iFlair) <dipen.modi@iflair.com>
2023-02-22 17:59:14 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var minify = require("gulp-minify");
+var browserSync = require("browser-sync").create();
+
+gulp.task("sass", function () {
+  return gulp
+    .src("source/scss/**/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("public/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("js", function () {
+  return gulp
+    .src("source/js/**/*.js")
+    .pipe(minify())
+    .pipe(gulp.dest("public/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("copyImage", function () {
+  return gulp.src("source/img/**/*.*").pipe(gulp.dest("public/img"));
+});
+
+gulp.task("copyFont", function () {
+  return gulp.src("source/font/**/*.*").pipe(gulp.dest("public/font"));
+});
+
+gulp.task("serve", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "/index.html",
+    },
+  });
+  gulp
+    .watch("source/**/*.scss", gulp.series(["sass"]))
+    .on("change", browserSync.reload);
+  gulp.watch("**/*.html").on("change", browserSync.reload);
+  gulp
+    .watch("source/**/*.js", gulp.series(["js"]))
+    .on("change", browserSync.reload);
+  gulp
+    .watch("source/img/**/*.*", gulp.series(["copyImage"]))
+    .on("change", browserSync.reload);
+  gulp
+    .watch("source/font/**/*.*", gulp.series(["copyFont"]))
+    .on("change", browserSync.reload);
+});
